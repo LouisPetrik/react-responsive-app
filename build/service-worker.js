6f60333fd99ce9f1c77c21cb53eba67f396acd99
LouisPetrik <louispetrik@aol.com>
2019-03-08 19:20:06 +0100

    github pages setup works

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/LouisPetrik/react-responsive-app/precache-manifest.ca90afe8dfc40cd99b685da2b983e24d.js"
+  "/react-responsive-app/precache-manifest.f5cae8142f1099106bb005bb316ff2b4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/LouisPetrik/react-responsive-app/index.html", {
+workbox.routing.registerNavigationRoute("/react-responsive-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
