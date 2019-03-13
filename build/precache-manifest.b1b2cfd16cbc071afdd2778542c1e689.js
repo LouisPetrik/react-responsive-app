70a20dd1219f8aeb63b9f7edfcf231a4a4926d0a
LouisPetrik <louispetrik@aol.com>
2019-03-13 16:11:14 +0100

    fixed bug which caused that navbar element were not selectable

---
+++ build/precache-manifest.b1b2cfd16cbc071afdd2778542c1e689.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "06001eb5d4c1e10a0e24",
+    "url": "/react-responsive-app/static/js/runtime~main.06001eb5.js"
+  },
+  {
+    "revision": "37b3f845d5197482dc22",
+    "url": "/react-responsive-app/static/js/main.37b3f845.chunk.js"
+  },
+  {
+    "revision": "6ecdb5428820a811f31d",
+    "url": "/react-responsive-app/static/js/2.6ecdb542.chunk.js"
+  },
+  {
+    "revision": "6ecdb5428820a811f31d",
+    "url": "/react-responsive-app/static/css/2.c439d946.chunk.css"
+  },
+  {
+    "revision": "bd8b606530522f4651d84388405213c7",
+    "url": "/react-responsive-app/index.html"
+  }
+];
