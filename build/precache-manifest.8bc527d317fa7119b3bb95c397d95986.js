92a1691c3515b05b8b109303fe8e68cffaed5e6f
LouisPetrik <louispetrik@aol.com>
2019-03-13 15:06:29 +0100

    refreshed build folder after completing the modal

---
+++ build/precache-manifest.8bc527d317fa7119b3bb95c397d95986.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "06001eb5d4c1e10a0e24",
+    "url": "/react-responsive-app/static/js/runtime~main.06001eb5.js"
+  },
+  {
+    "revision": "fbb415abe0045eec5311",
+    "url": "/react-responsive-app/static/js/main.fbb415ab.chunk.js"
+  },
+  {
+    "revision": "187bd5ecafb32e258afa",
+    "url": "/react-responsive-app/static/js/2.187bd5ec.chunk.js"
+  },
+  {
+    "revision": "187bd5ecafb32e258afa",
+    "url": "/react-responsive-app/static/css/2.c439d946.chunk.css"
+  },
+  {
+    "revision": "7988ffb22524ad541cf07c5a461a90d9",
+    "url": "/react-responsive-app/index.html"
+  }
+];
